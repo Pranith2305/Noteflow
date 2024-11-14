ddd45be4b154332c52f97634c553b1e0eb05674c
pranithaiwiz <pranithkumar.8802@gmail.com>
2024-11-14 12:17:08 +0530

    homesection added

--- eraser/src/components/ui/HeroSection.tsx
+++ eraser/src/components/ui/HeroSection.tsx
@@ -2,36 +2,38 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <section className="bg-gray-50">
-  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-    <div className="mx-auto max-w-xl text-center">
-      <h1 className="text-3xl font-extrabold sm:text-5xl">
-        Understand User Flow.
-        <strong className="font-extrabold text-red-700 sm:block"> Increase Conversion. </strong>
-      </h1>
+    <section className="bg-black">
+    <div className='flex items-baseline 
+    justify-center pt-20'>
+    <h2 className='text-white border 
+        px-3 p-2 rounded-full
+    text-center border-white'>See What's New | <span className='text-sky-300'>AI Diagram</span></h2>
 
-      <p className="mt-4 sm:text-xl/relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-        numquam ea!
-      </p>
-
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
-        <a
-          className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-          href="#"
-        >
-          Get Started
-        </a>
-
-        <a
-          className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-          href="#"
-        >
-          Learn More
-        </a>
-      </div>
     </div>
+<div className="mx-auto h-screen max-w-screen-xl px-4 py-12 lg:flex  ">
+<div className="mx-auto max-w-xl text-center">
+  <h1 className="text-3xl text-sky-300 font-extrabold sm:text-5xl">
+  Documents & diagrams
+    <strong className="font-extrabold text-white sm:block"> 
+    for engineering teams. </strong>
+  </h1>
+
+  <p className="mt-4 sm:text-xl/relaxed text-slate-200">
+  All-in-one markdown editor, collaborative canvas, and diagram-as-code builder
+  </p>
+
+  <div className="mt-8 flex flex-wrap justify-center gap-4">
+    <a
+      className="block w-full rounded bg-white text-black px-12 py-3 text-sm font-medium  shadow hover:bg-slate-600 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+      href="#"
+    >
+      Learn More
+    </a>
+
+   
   </div>
+</div>
+</div>
 </section>
   )
 }
